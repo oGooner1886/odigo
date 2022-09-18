e6757b048f70b87bce90bcc1f0a4b2f70949c1e9
oGooner1886 <work.gooner1886@gmail.com>
2022-09-18 22:23:49 +0400

    6gen(доделать футер, адаптив)

--- #source/js/index.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-        if (window.pageYOffset > 50) {
-            header.classList.add('.header_active')
-        }
-    };
-}());
-

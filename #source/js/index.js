77a6c73d07361e61c625a3dfe1db2cc3ee7be4ab
oGooner1886 <work.gooner1886@gmail.com>
2022-09-18 12:19:32 +0400

    5gen

---
+++ #source/js/index.js
@@ -0,0 +1,9 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('.header_active')
+        }
+    };
+}());
+
